fb10b96f7564de5bf1068d394b9a85cd15c925c5
vinicius_queiroz <viniciusqueiroz2106@gmail.com>
2025-06-29 21:10:53 -0300

    adicionando a classe exercicio e realocando os gifs pra posição correta

---
+++ musculacao/src/main/java/com/academia/model/Exercicio.java
@@ -0,0 +1,30 @@
+package com.academia.model;
+
+public class Exercicio {
+    private String nome;
+    private String descricao;
+    private String gifPath;
+
+    public Exercicio(String nome, String descricao, String gifPath) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.gifPath = gifPath;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getGifPath() {
+        return gifPath;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " - " + descricao + "\nGIF: " + gifPath;
+    }
+}
